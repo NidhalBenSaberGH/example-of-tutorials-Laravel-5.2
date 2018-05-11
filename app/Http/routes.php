d0bcf9c4bdacacbffd87ad7f89707a39c43d0ef7
nidhal.bensaber <nidhal.bensaber@outlook.com>
2018-05-11 18:31:18 +0100

    tuto230

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,5 +24,5 @@ Route::get('/home', 'HomeController@index');
 Route::group(['middleware'=>'admin'], function (){
 
     Route::resource('/admin/users', 'AdminUsersController');
-
+    Route::resource('/admin/posts', 'AdminPostsController');
 });
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <h1>Create Posts</h1>
+    <div class="col-sm-9">
+        {!! Form::open(['method'=>'POST', 'action'=>'AdminPostsController@store', 'files'=>true])  !!}
+        <div class="form-group">
+            {!! Form::label('title', 'Title:') !!}
+            {!! Form::text('title', null, [ 'class'=>'form-control' ]) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('category_id', 'Category:') !!}
+            {!! Form::select('category_id', [''=>'Choose options', 0=>'PHP', 1=>'JavaScript'],null, [ 'class'=>'form-control' ]) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('photo_id', 'File:') !!}
+            {!! Form::file('photo_id', null, [ 'class'=>'form-control' ]) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('body', 'Description:') !!}
+            {!! Form::textarea('body', null, [ 'class'=>'form-control', 'rows'=>3]) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+        </div>
+        {{ csrf_field() }}
+    {!! Form::close() !!}
+    @include('includes.form-error')
+
+@endsection
